db5a11a904123c7147afca04827d35abfd750b9f
Greg Thomas <gregoryjamesthomas@gmail.com>
2013-12-24 20:00:58 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<link href='http://fonts.googleapis.com/css?family=Lato|Istok+Web|Arimo|Source+Code+Pro:200|Muli:300' rel='stylesheet' type='text/css'>
+		<title>Greg Thomas</title>
+		<meta charset="utf-8"/>
+		<link href="style1.css" rel="stylesheet" type="text/css"/>
+		<script><!--google analytics-->
+  		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+ 		 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+ 		 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ 		 ga('create', 'UA-42862080-1', 'thegregthomas.com');
+ 		 ga('send', 'pageview');
+		</script>
+		<link rel="shortcut icon" href="illinois.ico">
+	</head>
+
+	<body class="home">
+
+		<div id="header">
+			<h1><a href="index.html">Greg Thomas</a></h1>	
+			<ul id="navigation">
+				<li><a href="index.html">Home</a></li>
+				<li><a href="listen.html">Listen</a></li>
+				<li><a href="photos.html">Photos</a></li>
+				<li><a href="charity.html">Charity</a></li>
+				<li><a href="contact.html">Contact</a></li>
+				<li class="logo"><a href="index.html"><img src="towercircle.png"></a></li>
+			</ul> <!--end of navigation ul-->
+		</div> <!--end of header div-->
+				<div id="footer">
+		
+			<div id="social">
+				<ul>
+					<li><a href="http://www.youtube.com/gregoryjmrmr" target="_blank"><img src="you_tube.png"></a><li>
+					<li><a href="http://www.facebook.com/gregthomasband" target="_blank"><img src="fb_1.png"></a><li>
+					<li><a href="http://www.twitter.com/gregoryjmrmr" target="_blank"><img src="twitter_2.png"></a><li>
+				</ul>
+			</div><!--end of social div-->
+	
+			<div id="signup">	
+				<form action="http://thegregthomas.us6.list-manage.com/subscribe/post?u=62449678d1f75debb3c18c9de&amp;id=a97c858ca4" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+					<div id="inputs">
+						<label for="mce-EMAIL"></label>
+						<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" 	required>
+					</div><!--end of inputs div-->
+					
+					<div class="button">
+						<input type="submit" value="Get Updates" name="subscribe" id="mc-embedded-subscribe" class="button">
+					</div><!--end of button div-->
+
+				</form>
+			</div><!--end of signup form-->
+			<!--End mc_embed_signup-->
+		</div><!--end of footer div-->	
+		<div id="content">
+			<div id="content-video">
+				<iframe src="http://player.vimeo.com/video/53749030" width="850" height="477.7" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+			</div><!--end of video div-->
+			<h2>Anchors out now - donate and help fund music programs</h2>
+			<br>
+			<p>Anchors is out today and available for download for whatever you'd like to pay.  Donate $1 or $5,000, and all of the money raised will go to fund music programs in need through Donors Choose.</p>
+			<br>
+			<p>Check out the <a href="http://igg.me/at/anchors" target=none>IndieGoGo page</a> to get your copy of Anchors and donate to a great cause.</p>
+
+		</div><!--end of content div-->
+	</body>
+</html>	
